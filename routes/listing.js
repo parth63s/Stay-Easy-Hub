c0715f93b6f8bdbb9265a9ecda1b309d9367ac87
Parth Ramani <parthramani63@gmail.com>
2024-07-28 15:24:40 +0530

    Add flash message (success and failuer) using Sessions.

--- routes/listing.js
+++ routes/listing.js
@@ -34,6 +34,10 @@ router.get("/new", (req,res) =>{
 router.get("/:id", wrapAsync(async (req, res)=> {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listing/show.ejs", {listing});
 }));
 
@@ -42,6 +46,7 @@ router.post("/",validateListing, wrapAsync(async (req, res, next)=> {
     const newListing = new Listing(req.body.listing);
     
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 }));
 
@@ -59,6 +64,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id",validateListing, wrapAsync( async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }))
 
@@ -68,6 +74,7 @@ router.delete("/:id",wrapAsync( async (req, res) => {
     let{id} = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 }))
 
