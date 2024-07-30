94f431f8062629b0165b105af8dc42acdd0bd5a8
Parth Ramani <parthramani63@gmail.com>
2024-07-30 18:49:45 +0530

    Implement Review Authorization

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 const Listing = require("./models/listing.js");
-const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
+const { listingSchema , reviewSchema} = require("./schema.js");
 const ExpressError = require("./utils/ExpressError.js");
+
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
@@ -50,4 +52,15 @@ module.exports.validateReview = (req, res, next) => {
     } else {
         next();
     }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let {id ,reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+
+    if(!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the author of this review")
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 }
--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams:true});
 const wrapAsync = require("../utils/wrapAsyc.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview, isLoggedIn} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 
@@ -24,7 +24,7 @@ router.post("/",isLoggedIn, validateReview,wrapAsync( async(req, res) => {
 
 
 // delete review route
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId", isLoggedIn,isReviewAuthor, wrapAsync(async (req, res) => {
     let {id, reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull : {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
