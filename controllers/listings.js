645f9ff688c8a9171680afd00dcdbde3d58f50dd
Parth Ramani <parthramani63@gmail.com>
2025-02-27 23:55:52 +0530

    Add Comment in Project

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,6 @@
 const Listing = require("../models/listing");
 
+// all listing
 module.exports.index = async (req,res) => {
     const searchText = req.query.search;
     const choice = req.query.choice;
@@ -14,10 +15,12 @@ module.exports.index = async (req,res) => {
     res.render("listing/index.ejs", {allListings, searchText, choice});
 }
 
+//render new listing page
 module.exports.renderNewForm = (req,res) =>{
     res.render("listing/new.ejs");
 }
 
+//render show listing page
 module.exports.showListing = async (req, res)=> {
     let {id} = req.params;
     const listing = await Listing.findById(id)
@@ -35,7 +38,7 @@ module.exports.showListing = async (req, res)=> {
     res.render("listing/show.ejs", {listing});
 }
 
-
+// add listing in database
 module.exports.createListing = async (req, res, next)=> {
     let url = req.file.path;
     let filename = req.file.filename;
@@ -48,6 +51,7 @@ module.exports.createListing = async (req, res, next)=> {
     res.redirect("/listings");
 }
 
+//render Edit page
 module.exports.renderEditForm = async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
@@ -60,6 +64,7 @@ module.exports.renderEditForm = async (req, res) => {
     res.render("listing/edit.ejs", {listing, originalImage});
 }
 
+//update listing in database
 module.exports.updateListing = async (req, res) => {
     let {id} = req.params;
     let listing  = await Listing.findByIdAndUpdate(id, {...req.body.listing});
@@ -73,6 +78,7 @@ module.exports.updateListing = async (req, res) => {
     res.redirect(`/listings/${id}`);
 }
 
+//delete listing in database
 module.exports.destroyListing = async (req, res) => {
     let{id} = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
